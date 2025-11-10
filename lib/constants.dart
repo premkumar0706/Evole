11f55191df34fbec0c26c96ce63eb3d9ba19a809
premkumar0706 <premkumar902645@gmail.com>
2025-11-10 17:47:58 +0530

    updated all changes

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,14 +1,53 @@
 import 'package:flutter/material.dart';
 
-const Color primaryColor = Color(0xFF080345);
-const Color midColor = Color(0xFF0A0357);
-const Color accentColor = Color(0xFF0F0488);
-const Color whiteColor = Colors.white;
-const Color lightWhite = Colors.white70;
 
-const double headingFontSize = 42.0;
-const double subtitleFontSize = 16.0;
-const double buttonFontSize = 18.0;
+const Color primaryColor = Color(0xFFFFFFFF); 
+const Color secondaryColor = Color(0xFF000000); 
+const Color backgroundColor = Colors.white;
 
-const double buttonBorderRadius = 30.0;
-const double defaultPadding = 16.0;
+
+const TextStyle headingTextStyle = TextStyle(
+  fontSize: 22,
+  fontWeight: FontWeight.bold,
+  color: Colors.black,
+);
+
+const TextStyle labelTextStyle = TextStyle(
+  fontSize: 15,
+  fontWeight: FontWeight.w500,
+  color: Colors.black,
+);
+
+const TextStyle hintTextStyle = TextStyle(
+  fontSize: 14,
+  color: Colors.grey,
+);
+
+const TextStyle buttonTextStyle = TextStyle(
+  fontSize: 16,
+  fontWeight: FontWeight.w600,
+  color: Colors.white,
+);
+
+
+BoxDecoration topLeftShapeDecoration = const BoxDecoration(
+  color: primaryColor,
+  borderRadius: BorderRadius.only(
+    bottomRight: Radius.circular(100),
+  ),
+);
+
+BoxDecoration bottomRightOuterDecoration = const BoxDecoration(
+  color: primaryColor,
+  borderRadius: BorderRadius.only(
+    topLeft: Radius.circular(60),
+  ),
+);
+
+BoxDecoration bottomRightInnerDecoration = const BoxDecoration(
+  color: secondaryColor,
+  borderRadius: BorderRadius.only(
+    topLeft: Radius.circular(80),
+  ),
+);
+const double buttonFontSize = 16.0;
