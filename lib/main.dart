09340ba551fadbd49daa4af791f20a7ff5cf396e
Divyanshu Sharma <divyanshu@gmail.com>
2025-12-01 20:11:40 +0530

    fix

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:evole/controller/userController.dart';
-import 'package:evole/homepage.dart';
+import 'package:evole/screens/homepage.dart';
 import 'package:evole/screens/basic.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
